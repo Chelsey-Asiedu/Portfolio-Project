5c00cce52d5065098d5c6c4df715e3f9ed4602e2
chelsey_a <chelsasiedu@gmail.com>
2024-08-27 16:06:38 -0500

    Coded js for indicator in nav menu and scroll button.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const gotoTopBtn= document.getElementById("go-to-top-button");
+
+let marker = document.querySelector("#marker");
+let item = document.querySelectorAll("nav li")
+
+function indicator(e){
+    marker.style.left = e.offsetLeft+"px";
+    marker.style.width = e.offsetWidth+"px";
+}
+
+item.forEach(link =>{
+    link.addEventListener("mouseenter", (e)=>{
+        indicator(e.target);
+    })
+})
+
+console.log(gotoTopBtn);
+
+// adding onscroll event listener to window element //
+window.onscroll = () => {
+    scrollFunction ();
+
+    function scrollFunction(){
+        if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+            gotoTopBtn.style.display="block";
+        } else{
+            gotoTopBtn.style.display="none";
+        }
+    }
+};
+gotoTopBtn.onclick=() => {
+    gotoTopBtn.style.display="none"
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    });
+        
+
+}
+
